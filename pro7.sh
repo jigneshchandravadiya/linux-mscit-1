5964291460b5beea339e8b16873ef02e17b4e55c
jigneshchandravadiya <jigneshchandravadiya581@gmail.com>
2026-09-16 17:09:41 +0530

    Add files via upload

---
+++ pro7.sh
@@ -0,0 +1,10 @@
+
+echo "hello world this is linux programming to store message using > greater than in output.txt file" > output.txt
+
+echo "this is linux programming to append message using >> double greater than in output.txt" >> output.txt
+
+cat < output.txt
+
+ls file_not_found 2> error.txt
+
+cat < error.txt
