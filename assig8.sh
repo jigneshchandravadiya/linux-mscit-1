8a0a390b10ab77706d3d153d8701c073e6f0f936
jigneshchandravadiya <jigneshchandravadiya581@gmail.com>
2026-09-24 17:47:28 +0530

    Add files via upload

---
+++ assig8.sh
@@ -0,0 +1,23 @@
+man#!/bin/bash
+
+if [ $# -eq 0 ]; then
+    echo "Usage: $0 file1 file2 file3 ..."
+    exit 1
+fi
+
+output="combined.txt"
+
+> "$output"
+
+for file in "$@"
+do
+    if [ -f "$file" ]; then
+        echo "*** hello : $file ***" >> "$output"
+        cat "$file" >> "$output"
+        echo "" >> "$output"
+    else
+        echo "Warning: $file is not a regular file."
+    fi
+done
+
+echo "All files concatenated into $output"
